f8fbf079eb1a266d050c6b2c39a663efbd98a5f1
PauFusco <99730051+PauFusco@users.noreply.github.com>
2024-05-13 01:31:12 +0200

    started working on radio dial
    the dial moves but not as expected

--- Assets/Resources/Scripts/DialController.cs
+++ Assets/Resources/Scripts/DialController.cs
@@ -1,19 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class DialController : MonoBehaviour
 {
-    [SerializeField] public float Current_X_Rotation;
-    
+    [SerializeField] float startRotationX;
+    [SerializeField] GameObject rightHand;
+    [SerializeField] XRBaseController controller;
+    [SerializeField] XRDirectInteractor controllerInteractor;
+
+    Vector3 startRotation;
+
     void Start()
     {
-        Current_X_Rotation = 0;
+        //startRotation = transform.rotation.eulerAngles;
+        //startRotation.x = startRotationX;
+
+        //transform.Rotate(startRotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (controllerInteractor.IsSelecting(GetComponent<XRBaseInteractable>()))
+        {
+            float newRotationX = rightHand.transform.rotation.eulerAngles.x;
+            Vector3 rotationToSet = transform.rotation.eulerAngles;
+            rotationToSet.x = newRotationX;
+
+            Debug.Log(rotationToSet);
+
+            transform.Rotate(rotationToSet);
+        }
     }
 }
