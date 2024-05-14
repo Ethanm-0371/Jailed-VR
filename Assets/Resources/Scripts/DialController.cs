18d995dcecb4d70d115d632c6d8be276c3f1ae4e
Ethanm-0371 <99729919+Ethanm-0371@users.noreply.github.com>
2024-05-14 11:35:25 +0200

    radio dial interaction corrected
    - rotation is now done with quaternions
    
    - dial works now
    
    Co-Authored-By: PauFusco <99730051+PauFusco@users.noreply.github.com>

--- Assets/Resources/Scripts/DialController.cs
+++ Assets/Resources/Scripts/DialController.cs
@@ -10,28 +10,13 @@ public class DialController : MonoBehaviour
 
     [SerializeField] float snapAmount;
 
-    void Start()
-    {
-        //startRotation = transform.rotation.eulerAngles;
-        //startRotation.x = startRotationX;
-
-        //transform.Rotate(startRotation);
-    }
-
     // Update is called once per frame
     void Update()
     {
         if (controllerInteractor.IsSelecting(GetComponent<XRBaseInteractable>()))
         {
-            float newRotationZ = rightHand.transform.rotation.eulerAngles.z;
-
-
-            Vector3 rotationToSet = transform.rotation.eulerAngles;
-            rotationToSet.x = newRotationZ;
-
-            //Debug.Log(rotationToSet);
-
-            transform.localRotation = ();
+            transform.localRotation = Quaternion.Euler(0, 0, 90);
+            transform.Rotate(Vector3.up * rightHand.transform.eulerAngles.z);
         }
     }
 }
