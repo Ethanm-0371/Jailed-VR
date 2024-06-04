c538e5ffa5f3085eded8add5828146d6ae0ead2a
Jonathan Cacay Llanes <jcacaynes@gmail.com>
2024-06-04 03:16:14 +0200

    Spooky scene hehehe

--- Assets/Resources/Scripts/NumpadPuzzle/UvLamp.cs
+++ Assets/Resources/Scripts/NumpadPuzzle/UvLamp.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class MagicLightSource : MonoBehaviour
 {
-    public Material reveal;
+    public Material[] matsToReveal;
     public Light uvLight;
 
     void Update()
     {
-        reveal.SetVector("_LightPosition", uvLight.transform.position);
-        reveal.SetVector("_LightDirection", -uvLight.transform.forward);
-        reveal.SetFloat("_LightAngle", uvLight.spotAngle);
+        foreach(Material mat in matsToReveal)
+        {
+            mat.SetVector("_LightPosition", uvLight.transform.position);
+            mat.SetVector("_LightDirection", -uvLight.transform.forward);
+            mat.SetFloat("_LightAngle", uvLight.spotAngle);
+        }
     }
 }
